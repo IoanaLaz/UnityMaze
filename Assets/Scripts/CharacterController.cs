0ce2a19aa3cd129f0be09769bb62b64dfa8bf663
IoanaLaz <ioana_lazaroae@yahoo.com>
2018-05-09 22:07:58 +0300

    Maze Player Enemy

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float tranlationY = Input.GetAxis("Vertical") * speed;
+		float translationX = Input.GetAxis("Horizontal") * speed;
+
+		//makes sure the character moves smoothly, deltaTime = time between updates
+		tranlationY *= Time.deltaTime;
+		translationX *= Time.deltaTime;
+
+		transform.Translate(translationX, 0, tranlationY);
+
+		if (Input.GetKeyDown("escape"))
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+	}
